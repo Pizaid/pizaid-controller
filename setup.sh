a8eb9507d9857d8396ec380f28e7d0b7e09beb24
Chikashi Shinagawa <cks.shina@gmail.com>
2014-03-01 04:30:30 +0900

    add chekck of machine

--- setup.sh
+++ setup.sh
@@ -1,16 +1,23 @@
 #! /bin/bash
 # setupper for Pizaid-Controller
 
+# check machine
+if [ `hostname` != "raspberrypi" ]; then
+    echo "This machine is not raspberrypi"
+    exit 1
+fi
+
 # check user
 if [ "` id | grep root`" = "" ]; then
     echo "Please re-run as super user"
     exit 1
 fi
+
 echo "start setup of Pizaid-Controller"
 
-
+echo "update and install depend packages"
 # update packages before install
-aptitude update 
+aptitude update
 aptitude upgrade
 
 # install lvm and filesisytems packages
