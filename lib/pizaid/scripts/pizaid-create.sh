14e9610c2615ae3cbb5fa46daacf6d6f634e080f
Chikashi Shinagawa <cks.shina@gmail.com>
2014-03-14 02:59:19 +0000

    add check

--- lib/pizaid/scripts/pizaid-create.sh
+++ lib/pizaid/scripts/pizaid-create.sh
@@ -19,13 +19,33 @@ if [ `hostname` != "raspberrypi" ]; then
 	exit 1
 fi
 
-
 # check user
 if [ "` id | grep root`" = "" ]; then
 	echo "Please re-run as super user"
 	exit 1
 fi
 
+# check volume name
+if [ "` echo $VNAME | grep '[^A-Za-z0-9]' `" != "" ]; then
+	echo "Unacceptable PizaidVolumeName: $VNAME (You can use A-Z,a-z and 0-9)"
+	exit 1
+fi
+
+if [ "` echo $VNAME | grep 'sync'`" != "" ]; then
+	echo "Unacceptable PizaidVolumeName: $VNAME (can not use "sync" for PizaidVolumeName)"
+	exit 1
+fi
+
+# check device
+if [ ! -f $DPATH ]; then
+	echo "$DPATH is not exist or file"
+	exit 1
+fi
+
+if [ "` echo $DPATH | grep '^/dev/sd[a-z]$' `" ]; then
+	echo "$DPATH is invalid ( /dev/sd[a-z] is valid )"
+	exit 1
+fi
 
 # create pertation for volume group
 fdisk $DPATH << EOF
