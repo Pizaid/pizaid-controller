7bf5a61d9edaf22dfcdc0f9dfd7efdb6ac51f82e
Makoto Shimazu <makoto.shimaz@gmail.com>
2014-08-01 18:11:54 +0900

    add network setting

--- lib/pizaid/dbusnetwork.rb
+++ lib/pizaid/dbusnetwork.rb
@@ -18,6 +18,27 @@ module Pizaid
             x.name == "eth0" and x.addr.ipv6?
           }.first.addr.ip_address
         end
+        dbus_method :Update_ipv4, "in ipstr:s, out result:b" do |ipstr|
+          return false if not validate_ipv4(ipstr)
+          rc = set_ipv4(ipstr)
+          self.StatusUpdated()
+          return rc
+        end
+        dbus_signal :StatusUpdated
+      end
+      def set_ipv4(str)
+        puts str
+        return true
+      end
+      def validate_ipv4(str)
+        return false if str.class != String
+        nums = str.scan(/^(\d+)\.(\d+)\.(\d+)\.(\d+)$/)
+        return false unless nums.length == 1
+        return false unless nums[0].length == 4
+        for n in nums[0] do
+          return false unless n.to_i >= 0 && n.to_i <= 255
+        end
+        return true
       end
     end
   end
