672ce0a8b70cbd7e90601dad104b34821e1e8fd1
Chikashi Shinagawa <cks.shina@gmail.com>
2014-08-09 05:11:52 +0900

    change to use struct

--- lib/pizaid/storage.rb
+++ lib/pizaid/storage.rb
@@ -2,42 +2,40 @@ module Pizaid
   module Controller
     class Storage
       def initialize
-        @names = ["main", "sync"]
+        storage = Struct::new('Storage', :name, :option, :devs)
+        @storages = [ storage.new(name: "main", option: "", devs: []),
+                      storage.new(name: "sync", option: "-S", devs: []),
+                      storage.new(name: "unused", option: "-U", devs: []) ]
         @script_dir = File.expand_path(File.dirname(__FILE__)) + "/scripts"
       end
       def get_names()
-        puts("get_names: #{@names}")
-        return @names
+        names = @storages.select { |storage| storage.name }
+        puts("get_names: #{names}")
+        return names
       end
       def get_capacity_kb(name)
         capacity = 0
-        case name
-        when @names[0]
-          capacity = `#{@script_dir}/pizaid-volume --size`.to_i
-        when @names[1]
-          capacity = `#{@script_dir}/pizaid-volume -S --size`.to_i
+        target = @storage.find{ |storage| storage.name == name }
+        if target != nil
+          capacity = `#{@script_dir}/pizaid-volume #{target.option} --size`.to_i
         end
         puts("get_capacity_kb: #{name}, #{capacity}")
         return capacity
       end
       def get_usage_kb(name)
         used = 0
-        case name
-        when @names[0]
-          used = `#{@script_dir}/pizaid-volume --use`.to_i
-        when @names[1]
-          used = `#{@script_dir}/pizaid-volume -S --use`.to_i
+        target = @storage.find{ |storage| storage.name == name }
+        if target != nil
+          used = `#{@script_dir}/pizaid-volume #{target.option} --use`.to_i
         end
         puts("Get_used: #{name}, #{used}")
         return used
       end
       def get_usage_percent(name)
         percent = 0
-        case name
-        when @names[0]
-          percent = `#{@script_dir}/pizaid-volume --use -p`.to_i
-        when @names[1]
-          percent = `#{@script_dir}/pizaid-volume -S --use -p`.to_i
+        target = @storage.find{ |storage| storage.name == name }
+        if target != nil
+          percent = `#{@script_dir}/pizaid-volume #{target.option} --use -p`.to_i
         end
         puts("Get_usage: #{name}, #{percent}")
         return percent
@@ -49,11 +47,9 @@ module Pizaid
       end
       def join(name, device)
         rc = false
-        case name
-        when @names[0]
-          rc = system("#{@script_dir}/pizaid-disk #{device}")
-        when @names[1]
-          rc = system("#{@script_dir}/pizaid-disk -S #{device}")
+        target = @storage.find{ |storage| storage.name == name }
+        if target != nil
+          rc = system("#{@script_dir}/pizaid-disk #{target.option} #{device}")
         end
         return rc
       end
