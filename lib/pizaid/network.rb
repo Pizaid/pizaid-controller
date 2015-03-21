0e12ab982f58927ee7071fa412b1970d72145c12
Chikashi Shinagawa <cks.shina@gmail.com>
2015-03-21 16:49:09 +0900

    network.rb: rescue when there is no ipv4/ipv6 address

--- lib/pizaid/network.rb
+++ lib/pizaid/network.rb
@@ -4,14 +4,25 @@ module Pizaid
   module Controller
     class Network
       def ipv4()
-          Socket.getifaddrs.select {|x|
+        ip_address = nil
+        begin
+          ip_address = Socket.getifaddrs.select {|x|
             x.name == "eth0" and x.addr.ipv4?
           }.first.addr.ip_address
+        rescue
+          ip_address = "NoIPV4"
+        end
       end
       def ipv6()
-          Socket.getifaddrs.select {|x|
+        ip_address = nil
+        puts "calledipv6"
+        begin
+          ip_address = Socket.getifaddrs.select {|x|
             x.name == "eth0" and x.addr.ipv6?
           }.first.addr.ip_address
+        rescue
+          ip_address = "NoIPV6"
+        end
       end
     end
   end
